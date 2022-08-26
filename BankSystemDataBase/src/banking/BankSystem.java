ceee01660254f9e4c4f0f1731c3c33368eef7f49
2000SnehaBS <110822116+2000SnehaBS@users.noreply.github.com>
2022-08-26 17:11:11 +0530

    Update BankSystem.java

--- BankSystemDataBase/src/banking/BankSystem.java
+++ BankSystemDataBase/src/banking/BankSystem.java
@@ -5,29 +5,92 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BankSystem{
-	static ResultSet rs = null;
-	public static void createCustomerAccount(String id, String firstName, String username, String lastName, String age, String address, String mobileNumber, String eMailId, String dateofbirth, String balance) {
+	public static ResultSet rs = null;
+	public static void createCustomerAccount(String customerid, String firstName, String username, String lastName, int age, String address,long mobileNumber, String eMailId, String dateofbirth, int balance) {
 		try {
-			String sql ="insert into customer(id,first_name,user_name,last_name,age,address,mobile_number,email_id,date_of_birth,balance) values(?,?,?,?,?,?,?,?,?,?)";
+			String sql ="insert into customer(customerid,first_name,user_name,last_name,age,address,mobile_number,email_id,date_of_birth,balance) values(?,?,?,?,?,?,?,?,?,?)";
 			PreparedStatement stmt = BankDb.conn.prepareStatement(sql);
-			stmt.setString(1, id);
+			stmt.setString(1, customerid);
 			stmt.setString(2, firstName);
 			stmt.setString(3, username);
 			stmt.setString(4, lastName);
-			stmt.setString(5, age);
+			stmt.setLong(5, age);
 			stmt.setString(6, address);
-			stmt.setString(7, mobileNumber);
+			stmt.setLong(7, mobileNumber);
 			stmt.setString(8, eMailId);
 			stmt.setString(9,dateofbirth);
-			stmt.setString(10, balance);
-			int rowsSelected = stmt.executeUpdate();
-			if(rowsSelected>0) {
-				System.out.println(" Account created successfully");
+			stmt.setLong(10, balance);
+			int rows = stmt.executeUpdate();
+			if(rows>0) {
+				System.out.println("Given information inserted successfully.....");
 			}
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
-			main(args);
+		}
+	}
+	
+	public static void depositMoney(String customerid,String username,int amt) {
+		try {
+		
+		PreparedStatement ps = BankDb.conn.prepareStatement("select balance from customer where customerid = ?");
+		ps.setString(1, customerid);
+		rs = ps.executeQuery();
+		rs.next();
+			
+		String sql ="update customer set balance = ? where customerid = ?";
+		PreparedStatement stmt = BankDb.conn.prepareStatement(sql);
+		stmt.setInt(1,(amt+rs.getInt(1)));
+		stmt.setString(2, customerid);
+		int rows = stmt.executeUpdate();
+		if(rows > 0) {
+			System.out.println("Money deposited successfully....The total balance is"+(amt+rs.getInt(1)));
+		}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void withDrawMoney(String customerid,String username,int amt) {
+		try {
+
+		PreparedStatement ps = BankDb.conn.prepareStatement("select balance from customer where customerid = ?");
+		ps.setString(1, customerid);
+		rs = ps.executeQuery();
+		rs.next();
+		
+		String sql ="update customer set balance = ? where customerid = ?";
+		PreparedStatement stmt = BankDb.conn.prepareStatement(sql);
+		stmt.setInt(1,(rs.getInt(1)-amt));
+		stmt.setString(2, customerid);
+		int rows = stmt.executeUpdate();
+		if(rows > 0 && amt < rs.getInt(1)) {
+			System.out.println("Money withdraw successfully....."+(rs.getInt(1)-amt));
+		}
+		else {
+			System.out.println("balance is more than the current balance");
+		}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void closeAccount(String customerid) {
+		try {
+			String sql ="delete from customer where customerid =?";
+			PreparedStatement stmt = BankDb.conn.prepareStatement(sql);
+			stmt.setString(1, customerid);
+			int rows = stmt.executeUpdate();
+			if(rows > 0) {
+				System.out.println("Account closed successfully.....");
+			}
+			else {
+				System.out.println("The customerid "+customerid+" is not present...Enter the valid customerid");
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
